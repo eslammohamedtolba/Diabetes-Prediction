ffb766ff0589322fcc7464298f80f0752cd7f1c8
Eslam <eslam3012mohamed@gmail.com>
2023-10-21 21:03:04 +0300

    modify Diabetes preditor file

--- Diabetes predictor.py
+++ Diabetes predictor.py
@@ -27,13 +27,8 @@ X = Diabetes_data.drop(columns='Outcome',axis=1)
 Y = Diabetes_data['Outcome']
 
 
-# handling Diabetes data
-scaler = StandardScaler()
-standardized_data = scaler.fit_transform(X)
-
-
 # split data into train and test data
-x_train,x_test,y_train,y_test = train_test_split(standardized_data,Y,train_size=0.7,random_state=2)
+x_train,x_test,y_train,y_test = train_test_split(X,Y,train_size=0.7,random_state=2)
 
 
 # apply algorithm on input and output to predict
@@ -57,11 +52,16 @@ input_data = (1,189,60,23,846,30.1,0.398,59) # input user data
 input_data_array = np.array(input_data)
 # convert 1D input array data into 2D
 input_data_array_2D = input_data_array.reshape(1,-1)
-# standardization process for user input
-standardized_input_data = scaler.transform(input_data_array_2D)
 
-if svcModel.predict(standardized_input_data)[0]==1:
+if svcModel.predict(input_data_array_2D)[0]==1:
     print("this person has Diabetes")
 else:
     print("this person doesn't have Diabetes")
 
+
+
+# Importing pickle module to help in saving the model
+import pickle as pk
+# Saving the model
+file_name = 'trained_model.sav'
+pk.dump(svcModel , open(file_name,'wb'))
